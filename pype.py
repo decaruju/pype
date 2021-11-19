62667bb953bf6498e5b4c4c9328d0bd55b2b3773
Julien de Carufel <jcarufel@cognibox.com>
2021-11-19 17:36:57 -0500

    odds

--- pype.py
+++ pype.py
@@ -45,7 +45,7 @@ def padd(lhs, rhs):
 def pmap(callback):
     @functionize
     def inner(array):
-        return map(callback, array)
+        return list(map(callback, array))
     return inner
 
 
@@ -59,10 +59,47 @@ def pprod(lhs, rhs=None):
     else:
         return lhs * rhs
 
+@functionize
+def peq(lhs, rhs=None):
+    if rhs is None:
+        @functionize
+        def inner(elem):
+            return elem == lhs
+        return inner
+    else:
+        return lhs == rhs
 
+@functionize
+def pmod(lhs, rhs=None):
+    if rhs is None:
+        @functionize
+        def inner(elem):
+            return elem % lhs
+        return inner
+    else:
+        return lhs % rhs
+
+@functionize
+def pfilter(callback):
+    if isinstance(callback, list):
+        return [x for x in callback if x]
+
+    @functionize
+    def inner(array):
+        return [x for x in array if callback(x)]
+    return inner
+
+
+
+# TODO make pmap implicit with arithmetic functions
 pdoublesum = pmap(pprod(2)) | preduce(padd)
+odds = pfilter(pmod(2) | peq(1))
 
+lst = list
+
+array = [1, 2, 3]
 a = [1, 2, 3] | pmap(pprod(2)) | preduce(padd)
 b = [1, 2, 3] | pdoublesum
+print(array | odds)
 print(a)
 print(b)
